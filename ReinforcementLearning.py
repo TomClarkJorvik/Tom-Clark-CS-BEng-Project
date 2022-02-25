88da30de1e87377256ddfc1406cb3fb1615ee4de
TomClarkYorkie <twangletoo@gmail.com>
2022-02-25 13:55:31 +0000

    Update ReinforcementLearning.py

--- ReinforcementLearning.py
+++ ReinforcementLearning.py
@@ -3,7 +3,14 @@
 
 import gym
 import tensorflow as tf
+from tf_agents.agents import tf_agent
 
 class MinimalSubstrateEnvironment:
     def __init__(self):
         self.data=1
+
+
+class MSAgent(tf_agent.TFAgent):
+    def __init__(self):
+        self.data=1
+        #super(SignAgent, self).__init__(time_step_spec=time_step_spec,action_spec=action_spec,policy=policy,collect_policy=policy,train_sequence_length=None)
