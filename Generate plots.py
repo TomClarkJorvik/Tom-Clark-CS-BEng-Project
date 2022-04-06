ddafab46dc5ac4230e8b68c8f368e38dd74bc6ad
TomClarkJorvik <100420752+TomClarkJorvik@users.noreply.github.com>
2022-04-06 14:43:42 +0100

    Update Generate plots.py

--- Generate plots.py
+++ Generate plots.py
@@ -2,9 +2,9 @@ import logBook_B
 import logBook_C
 equation = 2
 
-# l1 = logBook_B.logbook(equation)
-# l1.loadLogbook("logB2.txt")
-# l1.plot_obj_fitness_and_rewards()
+l1 = logBook_B.logbook(equation)
+l1.loadLogbook("logB2.txt")
+l1.plot_obj_fitness_and_rewards()
 
 l2 = logBook_B.logbook(equation,scalarLabel="Scalar Value",maxScalarValue=50)
 l2.loadLogbook("logB2-10dims.txt")
